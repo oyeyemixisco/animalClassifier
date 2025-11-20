2ddff9ca151f54739c5b179e512b0118fce72674
Oyeyemi Dare Azeez <31821751+oyeyemixisco@users.noreply.github.com>
2025-11-21 00:43:37 +0100

    Add gunicorn for Render deployment

--- app.py
+++ app.py
@@ -55,12 +55,3 @@ def predict():
 @app.route("/")
 def home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=False)
-
-import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-
